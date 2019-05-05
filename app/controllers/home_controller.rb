c9583775db7e09b0290618e3df6ed8659586b719
Inje Yun <dbsdlswp@naver.com>
2019-05-05 23:40:28 +0900

    genre automation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
+$genre = Genre.all
 class HomeController < ApplicationController
+  @@a = 0
   def index
     @str = params[:str]
+    
+    if @@a == 0
+      Genre.create(:name => "Humor")
+      Genre.create(:name => "Game")
+      Genre.create(:name => "News")
+      Genre.create(:name => "Issue")
+      Genre.create(:name => "Animal")
+      @@a = 1
+    end
   end
 end
